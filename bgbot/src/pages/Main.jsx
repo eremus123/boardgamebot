2c42d46a28dd2d4602e728d39e54e4603f662aa5
eremus <eremus97@hotmail.com>
2024-03-18 22:45:22 +0800

    Update Main.jsx

--- bgbot/src/pages/Main.jsx
+++ bgbot/src/pages/Main.jsx
@@ -4,7 +4,7 @@ import HotGames from "../components/HotGames";
 const Main = () => {
   return (
     <>
-  <h1>Welcome to BoardGameBot!</h1>;
+  <h1>Welcome to BoardGameBot!</h1>
   <h2>Check out the hottest games currently:</h2>
   <HotGames></HotGames>
   </>)
