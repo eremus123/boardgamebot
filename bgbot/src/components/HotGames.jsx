a16baa802412577b56a5c46c10cc6f3822d67cf8
eremus <eremus97@hotmail.com>
2024-03-18 21:14:17 +0800

    add hotgames feature

---
+++ bgbot/src/components/HotGames.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+const HotGames = () => {
+    const [hotGames, setHotGames] = useState([]);
+
+    const FetchHotGames = async () => {
+        try {
+            const res = await fetch('http://localhost:8080/https://www.boardgamegeek.com/xmlapi2/hot?boardgame');
+            const text = await res.text();
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(text, "text/xml");
+            const items = xmlDoc.getElementsByTagName('item');
+            const gamesList = Array.from(items).map((item, index) => ({
+                rank: index + 1,
+                name: item.getElementsByTagName('name')[0].getAttribute('value'),
+                yearpublished: item.getElementsByTagName('yearpublished')[0].getAttribute('value'),
+                thumbnail: item.getElementsByTagName('thumbnail')[0].getAttribute('value'),
+            }));
+            setHotGames(gamesList);
+        } catch (error) {
+            console.error("Error fetching hotgames:", error);
+        }
+    };
+
+    useEffect(() => {
+        FetchHotGames();
+    }, []);
+
+    return (
+        <div className="games-container">
+            {hotGames.map((game, index) => (
+                <div key={index} className="game-card">
+                    <h5 className="game-rank">Rank: {game.rank}</h5>
+                    <h6 className="game-name">{game.name} (Published In: {game.yearpublished})</h6>
+                    <img src={game.thumbnail}></img>
+                    <br></br>
+                    <br></br>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default HotGames;
