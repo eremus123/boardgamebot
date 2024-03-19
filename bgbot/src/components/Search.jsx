854146e45576e2b1c17c9d78aac04b524da722dc
eremus <eremus97@hotmail.com>
2024-03-19 12:47:41 +0800

    Update Search.jsx
    bug in the search

--- bgbot/src/components/Search.jsx
+++ bgbot/src/components/Search.jsx
@@ -1,34 +1,40 @@
 import React, { useState } from "react";
 
 const Search = (props) => {
- const [gameName, setGameName] = useState("");
- const [results, setResults] = useState([]);
+  const [gameName, setGameName] = useState("");
+  const [results, setResults] = useState([]);
 
- const searchGames = async () => {
+  const searchGames = async () => {
     try {
-        const res = await fetch(
-            "https://api.airtable.com/v0/appnFG2kbIVgZNH8a/boardgames?filterByFormula=AND(FIND('"+gameName+"', LOWER({gamename})) > 0, OR(FIND('"+props.group+"', {group})))", //max 100 records.......
-            {
-              method: "GET",
-              headers: {
-                Authorization:
-                  "Bearer pat4GDBKgsQnZPgiY.c451f2ce36ec83b5deaf0ffae6c9f073e44d9c5ee26d29b71b54edb92d249246", 
-                "Content-Type": "application/json", 
-              },
-            }
-          );
+      const res = await fetch(
+        "https://api.airtable.com/v0/appnFG2kbIVgZNH8a/boardgames?filterByFormula=AND(FIND('" +
+          gameName +
+          "', LOWER({gamename})) > 0, OR(FIND('" +
+          props.group +
+          "', {group})))", //max 100 records.......
+        {
+          method: "GET",
+          headers: {
+            Authorization:
+              "Bearer pat4GDBKgsQnZPgiY.c451f2ce36ec83b5deaf0ffae6c9f073e44d9c5ee26d29b71b54edb92d249246",
+            "Content-Type": "application/json",
+          },
+        }
+      );
       const data = await res.json();
-      if (data.records.length===0) {
-        setResults([{ message: "This game is not owned/wishlisted by this group." }]);
+      if (data.records.length === 0) {
+        setResults([
+          { message: "This game is not owned/wishlisted by this group." },
+        ]);
       } else {
         setResults(data.records);
       }
     } catch (err) {
       console.error(err);
     }
- };
+  };
 
- return (
+  return (
     <div>
       <input
         type="text"
@@ -39,13 +45,20 @@ const Search = (props) => {
       <button onClick={searchGames}>Search</button>
       <ul>
         {results.map((result, index) => (
-          <li key={index}><img src={result.fields.imageurl} />
-            {`${result.fields.gamename} (${result.fields.status} by ${result.fields.owner})`}
+          <li key={index}>
+            {result.message ? (
+              <span>{result.message}</span> //check if result is a message, then throw the msg, otherwise then generate the results
+            ) : (
+              <>
+                <img src={result.fields.imageurl} />
+                {`${result.fields.gamename} (${result.fields.status} by ${result.fields.owner})`}
+              </>
+            )}
           </li>
         ))}
       </ul>
     </div>
- );
+  );
 };
 
 export default Search;
